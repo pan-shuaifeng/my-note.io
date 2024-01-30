1520649e91e07de0f99ed055786f0631a331ce8a
pan-shuaifeng <1073880430@qq.com>
2024-01-30 16:33:00 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,13 +9,13 @@ yarn docs:build
 
 # 进入生成的文件夹
 
-cd docs/.vitepress/dist
+# cd docs/.vitepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:pan-shuaifeng/my-note.io.git master:main
+git push -f git@github.com:pan-shuaifeng/my-note.io.git main:main
 # git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
 
 cd -
