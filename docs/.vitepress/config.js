2d7914fb5efa62c7810a4fc4970a7eb88eff9924
pan-shuaifeng <1073880430@qq.com>
2024-01-30 16:15:15 +0800

    "部署action-git"

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,7 +11,7 @@
 module.exports = {
     title: '小潘的学习历程', // 网站标题
     description: '总结归纳学习中的知识', // 网站的描述
-    base: './', //  部署时的路径 默认 / ，使用二级地址 /base/
+    base: '/my-vitepress-note/', //  部署时的路径 默认 / ，使用二级地址 /base/
     head: [['link', { rel: 'icon', href: '/favicon.ico' }]],// 添加网站图标
     // 主题配置
     themeConfig: {
